449bec620f1fa4f5564e102e80eb4e724eb53978
Gabriel Albino <enggabrielalbino@gmail.com>
2022-09-04 11:50:40 -0300

    Adicionando plataforma JADE no RUNTIME

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Word From Main");
+		jade.core.Runtime rt = jade.core.Runtime.instance();
+		Profile profile = new ProfileImpl();
+		
+		profile.setParameter(Profile.PLATFORM_ID, "Simulador Sala de Aula");
+		profile.setParameter(Profile.CONTAINER_NAME, "Sala de aula");
+		
+		AgentContainer mainContainer = rt.createMainContainer(profile);
+		try {
+			 AgentController ac = mainContainer.createNewAgent("aluno",
+					                      "AlunoAgent",null);
+			 AgentController rma = mainContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
+			 ac.start();
+			 rma.start();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
