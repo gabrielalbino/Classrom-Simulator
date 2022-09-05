459be3e50220bcf20cacaa3fa69bcac2ba63c6c9
Arthur Tavares <arthur001123@gmail.com>
2022-09-04 22:39:54 -0300

    Adicionando verificação de quantidade de alunos conversando e aluno palestrinha

--- src/SalaDeAulaController.java
+++ src/SalaDeAulaController.java
@@ -21,22 +21,21 @@ public class SalaDeAulaController {
 	private jade.core.Runtime rt;
 	private AgentContainer mainContainer;
 	public JFrame frame;
-	
+
 	public SalaDeAulaController(int quantidadeAlunos) {
 		alunos = new ArrayList<AgentController>();
 		createAndShowJade(quantidadeAlunos);
-        //Create and set up the window.
-        frame = new JFrame("Simulador de sala de aula");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-
+		// Create and set up the window.
+		frame = new JFrame("Simulador de sala de aula");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 
 	}
-	
+
 	private int getAulaTipoConteudo() {
 		try {
 			ProfessorAgentInterface professor = this.professor.getO2AInterface(ProfessorAgentInterface.class);
@@ -48,55 +47,59 @@ public class SalaDeAulaController {
 		}
 		return 0;
 	}
-	
 
-	// Cria o runtime JADE e 
+	// Cria o runtime JADE e
 	private void createAndShowJade(int quantidadeAlunos) {
 		this.rt = jade.core.Runtime.instance();
 		Profile profile = new ProfileImpl();
-		
+
 		profile.setParameter(Profile.PLATFORM_ID, "Simulador Sala de Aula");
 		profile.setParameter(Profile.CONTAINER_NAME, "Sala de aula");
-		profile.setParameter("services",Profile.DEFAULT_SERVICES + ";jade.core.messaging.TopicManagementService");
-		
+		profile.setParameter("services", Profile.DEFAULT_SERVICES + ";jade.core.messaging.TopicManagementService");
+
 		this.mainContainer = rt.createMainContainer(profile);
 		try {
 
-			interfaceController = mainContainer.createNewAgent("interface","agents.InterfaceAgent",null);
+			interfaceController = mainContainer.createNewAgent("interface", "agents.InterfaceAgent", null);
 			interfaceController.start();
-			 
-			for(int i = 0; i < quantidadeAlunos; i++) {
-				 AgentController ac = mainContainer.createNewAgent("Aluno " + i,"agents.AlunoNerdAgent",null);
-				 ac.start();
-				 alunos.add(ac);
+
+			for (int i = 0; i < quantidadeAlunos; i++) {
+				String tipoAluno;
+				if (i < 5) {
+					tipoAluno = "agents.AlunoPalestrinhaAgent";
+				} else {
+					tipoAluno = "agents.AlunoNerdAgent";
+				}
+
+				AgentController ac = mainContainer.createNewAgent("Aluno " + i, tipoAluno, null);
+				ac.start();
+				alunos.add(ac);
 			}
 
-			professor = mainContainer.createNewAgent("professor","agents.ProfessorAgent",null);
+			professor = mainContainer.createNewAgent("professor", "agents.ProfessorAgent", null);
 			professor.start();
-			 
-			
+
 			AgentController rma = mainContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
 			rma.start();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private void createAndShowGUI() {
-        //Add the ubiquitous "Hello World" label.
+	/**
+	 * Create the GUI and show it. For thread safety,
+	 * this method should be invoked from the
+	 * event-dispatching thread.
+	 */
+	private void createAndShowGUI() {
+		// Add the ubiquitous "Hello World" label.
 
-        JLabel labelProfessor = new JLabel("Aula: " + getAulaTipoConteudo());
-        frame.getContentPane().add(labelProfessor);
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+		JLabel labelProfessor = new JLabel("Aula: " + getAulaTipoConteudo());
+		frame.getContentPane().add(labelProfessor);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
 
 }
--- src/agents/ProfessorAgent.java
+++ src/agents/ProfessorAgent.java
@@ -1,4 +1,7 @@
 package agents;
+
+import java.util.Map;
+
 import agents.interfaces.ProfessorAgentInterface;
 import constants.StatusAula;
 import constants.Time;
@@ -16,90 +19,139 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.proto.AchieveREInitiator;
+import jade.lang.acl.MessageTemplate;
 
-public class ProfessorAgent extends Agent  implements ProfessorAgentInterface {
+public class ProfessorAgent extends Agent implements ProfessorAgentInterface {
 	private static final long serialVersionUID = -7358105736294185663L;
 	private int tipoConteudo;
+	private int conteudoPausado;
 
-	protected AID aulaTopic, notasTopic, updateTopic;
+	protected AID aulaTopic, alunosTopic, topicUpdateAlunos, notasTopic, updateTopic;
+
 	public ProfessorAgent() {
-		// Register the interfaces that must be accessible by an external program through the O2A interface
+		// Register the interfaces that must be accessible by an external program
+		// through the O2A interface
 		registerO2AInterface(ProfessorAgentInterface.class, this);
 	}
-	
+
 	protected void setup() {
 		try {
 			tipoConteudo = StatusAula.INICIANDO;
-			
+
 			registerAulaService();
-	
-			//registrando topic da aula
+
+			// registrando topic da aula
 			TopicManagementHelper topicHelper = (TopicManagementHelper) getHelper(TopicManagementHelper.SERVICE_NAME);
 
 			aulaTopic = topicHelper.createTopic(Topics.AULA);
+			alunosTopic = topicHelper.createTopic(Topics.ALUNOS);
+			topicUpdateAlunos = topicHelper.createTopic(Topics.UPDATE_DATA_ALUNOS);
 			notasTopic = topicHelper.createTopic(Topics.COMPUTA_NOTA);
 			updateTopic = topicHelper.createTopic(Topics.UPDATE_INTERFACE_REQUEST);
+			topicHelper.register(alunosTopic);
 			final TickerBehaviour contentUpdaterBehaviour = new TickerBehaviour(this, Time.AULA_TIME_STEP) {
-		        private static final long serialVersionUID = 7053736115204224490L;
-	
+				private static final long serialVersionUID = 7053736115204224490L;
+
 				protected void onTick() {
-					if(tipoConteudo == StatusAula.CONTEUDO_INTERESSANTE) {
-						tipoConteudo = StatusAula.CONTEUDO_IRRELEVANTE;
+					System.out.println("Status da Aula: " + tipoConteudo);
+					if (tipoConteudo == StatusAula.RECEBENDO_PALESTRINHA
+							|| tipoConteudo == StatusAula.CHAMANDO_ATENCAO) {
+						tipoConteudo = conteudoPausado;
+						System.out.println("Retornando conteúdo (id: " + tipoConteudo + ")");
+						return;
 					}
-					else {
+					if (tipoConteudo == StatusAula.CONTEUDO_INTERESSANTE) {
+						tipoConteudo = StatusAula.CONTEUDO_IRRELEVANTE;
+					} else {
 						tipoConteudo = StatusAula.CONTEUDO_INTERESSANTE;
 					}
 					ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
 					msg.addReceiver(aulaTopic);
 					msg.setContent("" + tipoConteudo);
 					myAgent.send(msg);
-					System.out.println("Novo conteúdo (id: " + tipoConteudo + ")" );
-					/* Agenda a ação de atualizar notas*/
+					System.out.println("Novo conteúdo (id: " + tipoConteudo + ")");
+					/* Agenda a ação de atualizar notas */
 					myAgent.addBehaviour(notasUpdaterBehaviour());
-					
-		        } 
+					myAgent.addBehaviour(classCheckerBehaviour());
+				}
 			};
-			
-			
-			//Comportamento de update do tipo de conteudo
-		    addBehaviour(contentUpdaterBehaviour);
-		    addBehaviour(requestInterfaceUpdate());
+
+			// Comportamento de update do tipo de conteudo
+			addBehaviour(contentUpdaterBehaviour);
+			addBehaviour(requestInterfaceUpdate());
 		} catch (ServiceException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private 
-	final WakerBehaviour notasUpdaterBehaviour() {
-		return (
-			new WakerBehaviour(this, Time.AULA_TIME_STEP/2) {
 
-		    private static final long serialVersionUID = 1L;
-	
-		    protected void handleElapsedTimeout() {
+	private final WakerBehaviour classCheckerBehaviour() {
+		return (new WakerBehaviour(this, Time.AULA_TIME_STEP / 3) {
+
+			private static final long serialVersionUID = 1L;
+
+			protected void handleElapsedTimeout() {
+				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+				msg.setContent("" + tipoConteudo);
+				msg.addReceiver(topicUpdateAlunos);
+				myAgent.send(msg);
+				System.out.println("Sending  ");
+				ACLMessage alunosMsg = myAgent.receive(MessageTemplate.MatchTopic(alunosTopic));
+				if (alunosMsg != null) {
+					String[] content = alunosMsg.getContent().split("/");
+					int alunosDispersos = Integer.parseInt(content[0]);
+					int palestrinha = Integer.parseInt(content[1]);
+					System.out.println("Palestrinhas " + palestrinha + " Dispersos" + alunosDispersos);
+					if (palestrinha == 1) {
+						if (tipoConteudo == StatusAula.CONTEUDO_INTERESSANTE
+								|| tipoConteudo == StatusAula.CONTEUDO_INTERESSANTE) {
+							conteudoPausado = tipoConteudo;
+						}
+						tipoConteudo = StatusAula.RECEBENDO_PALESTRINHA;
+						if (alunosDispersos > 5) {
+							if (tipoConteudo == StatusAula.CONTEUDO_INTERESSANTE
+									|| tipoConteudo == StatusAula.CONTEUDO_INTERESSANTE) {
+								conteudoPausado = tipoConteudo;
+							}
+							tipoConteudo = StatusAula.CHAMANDO_ATENCAO;
+						}
+					} else {
+						System.out.println("Null no professor ");
+						block();
+					}
+				}
+			}
+		});
+	}
+
+	private final WakerBehaviour notasUpdaterBehaviour() {
+		return (new WakerBehaviour(this, Time.AULA_TIME_STEP / 2) {
+
+			private static final long serialVersionUID = 1L;
+
+			protected void handleElapsedTimeout() {
 				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
 				msg.setContent("" + tipoConteudo);
 				msg.addReceiver(notasTopic);
 				myAgent.send(msg);
-		      } 
+			}
 		});
 	}
-	
 
-	// Retorna um mapa com o nome de cada aluno como chave e a nota e o status como valores.
-	private TickerBehaviour requestInterfaceUpdate(){
-		return (new TickerBehaviour(this, Time.AULA_TIME_STEP/10) {
-	        private static final long serialVersionUID = 7053736115204224490L;
+	// Retorna um mapa com o nome de cada aluno como chave e a nota e o status como
+	// valores.
+	private TickerBehaviour requestInterfaceUpdate() {
+		return (new TickerBehaviour(this, Time.AULA_TIME_STEP / 10) {
+			private static final long serialVersionUID = 7053736115204224490L;
 
 			protected void onTick() {
 				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
 				msg.addReceiver(updateTopic);
 				myAgent.send(msg);
-	        } 
+			}
 		});
 	}
-	
+
 	private void registerAulaService() {
 
 		DFAgentDescription dfd = new DFAgentDescription();
@@ -107,9 +159,9 @@ public class ProfessorAgent extends Agent  implements ProfessorAgentInterface {
 		ServiceDescription sd = new ServiceDescription();
 		sd.setName("sv-professor");
 		sd.setType("aula-forecast");
-		
+
 		dfd.addServices(sd);
-		
+
 		try {
 			DFService.register(this, dfd);
 		} catch (FIPAException e) {
@@ -117,19 +169,18 @@ public class ProfessorAgent extends Agent  implements ProfessorAgentInterface {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private DFAgentDescription[] getAlunosFromYellowPages() {
 		try {
-	  		// Build the description used as template for the search
-	  		DFAgentDescription template = new DFAgentDescription();
-	  		ServiceDescription templateSd = new ServiceDescription();
-	  		templateSd.setType("weather-forecast");
-	  		template.addServices(templateSd);
-	  		
-	  		DFAgentDescription[] results = DFService.search(this, template);
-	  		return results;
-	  	}
-		catch (Exception e) {
+			// Build the description used as template for the search
+			DFAgentDescription template = new DFAgentDescription();
+			ServiceDescription templateSd = new ServiceDescription();
+			templateSd.setType("weather-forecast");
+			template.addServices(templateSd);
+
+			DFAgentDescription[] results = DFService.search(this, template);
+			return results;
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
@@ -140,5 +191,5 @@ public class ProfessorAgent extends Agent  implements ProfessorAgentInterface {
 		// TODO Auto-generated method stub
 		return tipoConteudo;
 	}
-	
+
 }
